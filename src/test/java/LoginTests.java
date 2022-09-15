567a62472966ba3e540642d0e93cba1485b39ba1
Evnikel <evnikel@gmail.com>
2022-09-15 21:14:52 +0300

    added new class in main

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,46 +1,26 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
 public class LoginTests extends TestBase {
 
 
+    @BeforeMethod
+    public void precondition() {
+        if (app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+       }
+    }
+
+
     @Test
     public void loginSuccess() {
 
         // open login form
-        WebElement loginTab = wd.findElement(By.xpath("//a[@href='/login']"));
-        loginTab.click();
-        pause(3000);
-        //fill email evnikel@gmail.com Elena1234$@
-        WebElement inputEmail = wd.findElement(By.xpath("//input[@placeholder='Email']"));
-        inputEmail.click();
-        inputEmail.clear();
-        inputEmail.sendKeys("evnikel@gmail.com");
-        pause(3000);
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("evnikel@gmail.com" ,"Elena1234$@");
+        app.getHelperUser().sabmitLogin();
 
-
-        WebElement inputPassword = wd.findElement(By.xpath("//input[@placeholder='Password']"));
-        inputPassword.click();
-        inputPassword.clear();
-        inputPassword.sendKeys("Elena1234$@");
-        pause(3000);
-
-        // submit login
-        WebElement loginButton = wd.findElement(By.xpath("//*[text()=' Login']"));
-        loginButton.click();
-        pause(3000);
-
-    }
-
-    @Test
-    public void loginNegativeWrongEmailFormat() {
-
-    }
-
-    @Test
-    public void loginNegativeWrongPasswordFormat() {
 
     }
 
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,33 +1,21 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-
-import java.time.Duration;
+import manager.ApplicationManager;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
-    WebDriver wd;
 
-    @BeforeTest
-    public void init() {
-        wd = new ChromeDriver();
-        wd.manage().window().maximize(); /// open full screen
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/");
+    static ApplicationManager app = new ApplicationManager();
+
+    @BeforeSuite
+    public void setUp() {
+        app.init();
     }
 
 
-    @AfterTest
+    @AfterSuite
     public void tearDown() {
-        wd.quit();
+        app.stop();
+
     }
 
-
-    public void pause(int time) {
-        try {
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
