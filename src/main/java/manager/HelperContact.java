6bd35c4d7e051764d099df3e3273a28af5ad69a3
Evnikel <evnikel@gmail.com>
2022-10-05 19:01:06 +0300

    added RemoveContact

--- src/main/java/manager/HelperContact.java
+++ src/main/java/manager/HelperContact.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class HelperContact extends HelperBase {
 
@@ -24,9 +28,9 @@ public class HelperContact extends HelperBase {
         type(By.cssSelector("[placeholder='Name']"), contact.getName());
         type(By.cssSelector("[placeholder='Last Name']"), contact.getLastName());
         type(By.cssSelector("[placeholder='Phone']"), contact.getPhone());
-        type(By.cssSelector("[placeholder='email']"),contact.getEmail());
-        type(By.cssSelector("[placeholder='Address']"),contact.getAddress());
-        type(By.cssSelector("[placeholder='description']"),contact.getDescription());
+        type(By.cssSelector("[placeholder='email']"), contact.getEmail());
+        type(By.cssSelector("[placeholder='Address']"), contact.getAddress());
+        type(By.cssSelector("[placeholder='description']"), contact.getDescription());
     }
 
     public void saveContact() {
@@ -41,17 +45,17 @@ public class HelperContact extends HelperBase {
 
     public boolean isContactAddedByName(String name) {
         List<WebElement> list = wd.findElements(By.cssSelector("h2"));
-        for(WebElement el :list) {
+        for (WebElement el : list) {
             if (el.getText().equals(name))
                 return true;
-            }
-
-            return false;
         }
+
+        return false;
+    }
 
     public boolean inContactAddedByPhone(String phone) {
         List<WebElement> list = wd.findElements(By.cssSelector("h3"));
-        for(WebElement el :list) {
+        for (WebElement el : list) {
             if (el.getText().equals(phone))
                 return true;
         }
@@ -61,6 +65,67 @@ public class HelperContact extends HelperBase {
     }
 
     public boolean isAddPageStillDisplayed() {
-        return wd.findElements(By.cssSelector("a.active[href='/add']")).size()>0;
+        return wd.findElements(By.cssSelector("a.active[href='/add']")).size() > 0;
+    }
+
+
+    public int removeFirstContact() {
+        int countBefore = countOfContacts();
+        logger.info("Count before remove is " + countBefore);
+        if (!isContactListEmpty()) {
+            logger.info("List of Contact not Empty");
+            click(By.cssSelector(".contact-item_card__2SOIM"));
+            click(By.xpath("//button[text()='Remove']"));
+            pause(500);
+
+        }
+
+        int countAfter = countOfContacts();
+        logger.info("Count after remove is " + countAfter);
+        return countBefore - countAfter;
+    }
+
+
+    private int countOfContacts() {
+        return wd.findElements(By.cssSelector(".contact-item_card__2SOIM")).size();
+    }
+
+    private boolean isContactListEmpty() {
+        return wd.findElements(By.cssSelector(".contact-item_card__2SOIM")).isEmpty();
+    }
+
+    public void removeAllContacts() {
+        while (countOfContacts() != 0) {
+            click(By.cssSelector(".contact-item_card__2SOIM"));
+            click(By.xpath("//button[text()='Remove']"));
+            pause(500);
+        }
+    }
+
+
+    public boolean isNoContactsHere() {
+        return new WebDriverWait(wd, Duration.ofSeconds(5))
+                .until(ExpectedConditions
+                        .textToBePresentInElement(wd.findElement(By.cssSelector(".contact-page_message__2qafk h1")), "No Contacts here!"));
+    }
+
+
+    public void AddCountacts() {
+        Random random = new Random();
+        int i = random.nextInt(1000)+1000;
+
+        Contact contact = Contact.builder()
+                .name("Lisa"+i)
+                .lastName("Boi")
+                .phone("12345"+i)
+                .email("lisa" + i +"@gmail.com")
+                .address("Israel")
+                .description("My best friend")
+                .build();
+        openContactForm();
+        fillContactAllForm(contact);
+        clickTab();
+        saveContact();
     }
 }
+
---
+++ src/test/java/RemoveContact.java
@@ -0,0 +1,35 @@
+import models.Contact;
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RemoveContact extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        if (!app.getHelperUser().isLogged()) {
+            app.getHelperUser().login(new User().withEmail("evnikel@gmail.com").withPassword("Elena1234$@"));
+            logger.info("User login data: email evnikel@gmail.com & password Elena1234$@ ");
+        }
+
+        app.helperContact().AddCountacts();
+
+    }
+
+    @Test
+    public void removeFirstContact(){
+        Assert.assertEquals(app.helperContact().removeFirstContact(),1);
+        logger.info("Assert removeFirstContact ----> CountAfter");
+
+    }
+
+    @Test
+    public void removeAllContacts(){
+        app.helperContact().removeAllContacts();
+        Assert.assertTrue(app.helperContact().isNoContactsHere());
+        logger.info("Assert -removeAllContacts----> CountAfter ");
+
+
+    }
+}
